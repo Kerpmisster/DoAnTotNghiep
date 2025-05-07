8447e3bebce575605766f6bba6038af616a56291
NguyenTuanAnh <142615471+Kerpmisster@users.noreply.github.com>
2025-05-08 01:16:12 +0700

    commit-7/5/2025

---
+++ NoiThatHoangGia/NoiThatHoangGia/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NoiThatHoangGia.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
